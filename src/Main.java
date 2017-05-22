161283d55beb8d3b2bb8c55ebe4d3a011afb0a24
Roki <m.roki93@gmail.com>
2017-05-22 13:44:23 +0200

    Almost finally version5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 
-public class Main{
+public class Main {
 
-public static void main(String[]args){
-    JFrame frame1 = new MainFrame("Gracz w Kółko (O) i Krzyżyk (X). Pierwszy zaczyna gracz Kółko (O).");
-}
+    public static void main(String[] args) {
+        JFrame frame1 = new MainFrame("Gracz w Kółko (O) i Krzyżyk (X). Pierwszy zaczyna gracz Kółko (O).");
+    }
 }
 
 
